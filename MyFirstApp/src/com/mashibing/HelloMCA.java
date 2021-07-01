cb98aab87840d6524d593d852d6033ac5c5788d9
yangy55 <mltj_studio@163.com>
2021-07-02 00:59:22 +0800

    commit for the first time

---
+++ MyFirstApp/src/com/mashibing/HelloMCA.java
@@ -0,0 +1,14 @@
+package com.mashibing;
+
+/**
+ * @Author: mltj_studio
+ * @Date: 2021/6/28 - 06 - 28 - 00:37
+ * @Description: com.mashibing
+ * @Version: 1.0
+ */
+public class HelloMCA {
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+        new SecondHello();
+    }
+}
---
+++ MyFirstApp/src/com/mashibing/SecondHello.java
@@ -0,0 +1,14 @@
+package com.mashibing;
+
+import java.io.Serializable;
+
+/**
+ * @Author: mltj_studio
+ * @Date: 2021/6/28 - 06 - 28 - 00:40
+ * @Description: com.mashibing
+ * @Version: 1.0
+ */
+public class SecondHello implements Serializable {
+
+    private static final long serialVersionUID = -1469112502215614905L;
+}
